0538058d04ba396473ad0a5b9fce4b4332fddf6a
SparshSingh10 <sparsh262002@gmail.com>
2025-03-06 14:48:43 +0530

    first commit

--- src/main/java/com/example/EmployeePayrollApp/model/Employee.java
+++ src/main/java/com/example/EmployeePayrollApp/model/Employee.java
@@ -1,65 +1,46 @@
 package com.example.EmployeePayrollApp.model;
 
-
+import com.example.EmployeePayrollApp.dto.EmployeePayrollDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(name="employees")
-
+@Table(name = "employees")
 public class Employee {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @NotBlank(message = "Name should not be empty")
-        private String name;
+    @NotBlank(message = "Name should not be empty")
+    private String name;
 
-        @Min(value = 10000, message = "Salary should be greater than 0")
-        private double salary;
+    @Min(value = 10000, message = "Salary should be greater than 10000")
+    private double salary;
 
-        @NotBlank(message = "Department should not be empty")
-        private String department;
+    @NotBlank(message = "Department should not be empty")
+    private String department;
 
     public Employee() {}
 
-        public Employee(String name,double salary,String department){
-            this.name=name;
-            this.salary=salary;
-            this.department=department;
-        }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Employee(String name, double salary, String department) {
         this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
         this.salary = salary;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
         this.department = department;
     }
-}
 
+    public Employee(EmployeePayrollDTO employeePayrollDTO) {
+        this.name = employeePayrollDTO.getName();
+        this.salary = employeePayrollDTO.getSalary();
+        this.department = employeePayrollDTO.getDepartment();
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public double getSalary() { return salary; }
+    public void setSalary(double salary) { this.salary = salary; }
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+}
